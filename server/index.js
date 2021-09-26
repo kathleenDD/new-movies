b056637619a3c3a12d4bfe5dafb3c7108a5ebe76
kathleenDD <kathleen.domingo@datadoghq.com>
2021-09-26 13:27:16 +0200

    testing backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const API_URL =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=30f97dabd23a55145e8c6e9693269e9f&language=en-US&page=1";
+  // const API_URL =
+  //   "https://api.themoviedb.org/3/movie/popular?api_key=30f97dabd23a55145e8c6e9693269e9f&language=en-US&page=1";
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.send(API_URL.results);
   });
 
 app.listen(PORT, () => {
